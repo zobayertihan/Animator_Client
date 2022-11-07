42daa40abf41f27d2dc643d07e5d7f145a439d2d
zobayertihan <zobayertihan@gmail.com>
2022-11-08 02:35:01 +0600

    Route Added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import { RouterProvider } from 'react-router-dom';
+import router from './Components/Routes/Routes';
 
 function App() {
   return (
-    <div className="App">
+    <div>
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
